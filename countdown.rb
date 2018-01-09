c35e31ec0c774aa58d8047e5513366d946573f3a
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 21:28:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(integer)
   while integer > 0
     puts "#{integer} SECOND(S)!"
     integer -= 1
-  return "HAPPY NEW YEAR!"
   end
+  return "HAPPY NEW YEAR!"
 end
 binding.pry
 def countdown_with_sleep(number)
