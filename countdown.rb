cbc3fb20d1f590f60a00ed7a4d1176998e08931a
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 21:37:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,10 @@ def countdown(integer)
     puts "#{integer} SECOND(S)!"
     integer -= 1
   end
-  return "HAPPY NEW YEAR!"
+  if integer == 0
+    return "HAPPY NEW YEAR!"
 end
-
+end
 countdown(10)
 #def countdown_with_sleep(number)
 
