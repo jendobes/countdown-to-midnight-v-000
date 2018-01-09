04f42bdd3353e4140930622e773af382b8d7c6e4
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 21:29:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,9 @@ def countdown(integer)
   while integer > 0
     puts "#{integer} SECOND(S)!"
     integer -= 1
+    return "HAPPY NEW YEAR!"
   end
-  return "HAPPY NEW YEAR!"
+
 end
 
 countdown(10)
