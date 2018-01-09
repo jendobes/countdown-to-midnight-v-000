5cf8f08b09a28d1945c78cc910ae465737ca1e6c
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 21:25:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,15 @@
 #write your code here
-
+require pry
 def countdown(integer)
   integer -= 1
   while integer > 0
     puts "#{integer} SECOND(S)!"
   return "HAPPY NEW YEAR!"
+  binding.pry
   end
 
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
 
 end
